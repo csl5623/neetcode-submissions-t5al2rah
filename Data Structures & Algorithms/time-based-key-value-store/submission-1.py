b4985deb8f870c57827b8d07e14d75dbf55f33e8
Carla Lopez <105525582+csl5623@users.noreply.github.com>
2026-07-07 21:08:34 -0400

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.values = dict()
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.values:
+            self.values[key] = []
+        self.values[key].append((value,timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        max_value = ""
+        if len(self.values) > 0:
+            if key in self.values:
+                values = self.values[key]
+                max_timestamp = values[0][1]
+                for v,t in values:
+                    if t<=timestamp:
+                        if t >= max_timestamp:
+                            max_timestamp = t
+                            max_value = v
+                            print(v)
+                return max_value
+        return max_value
