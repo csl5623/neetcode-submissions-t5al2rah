d9154507af7a6dd455fbb266bedbbac57a36d387
Carla Lopez <105525582+csl5623@users.noreply.github.com>
2026-07-09 20:04:08 -0400

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited_nodes = set()
+        while head:
+            if head in visited_nodes:
+                return True
+            visited_nodes.add(head)
+            head = head.next
+        return False 
